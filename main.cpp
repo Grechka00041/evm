9bdcf97c3300c07dac6ed150a6ee5e44aed8232e
Grechka00041 <irinamuzycenko34@gmail.com>
2025-09-29 15:19:10 +0300

    lab2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include <conio.h>
-#include <stdio.h>
 #include <dos.h>
 
-void scroll(int direction, char l_row, char l_col, char r_row, char r_col, char attr)
+void scrollDown(char l_row, char l_col, char r_row, char r_col, char attr)
 {
     union REGS r;
-    if (direction) {
-        r.h.ah = 6;
-        r.h.al = direction;
-    } else {
-        r.h.ah = 6;
-        r.h.al = 0;
-    }
+    r.h.ah = 0x07;
+    r.h.al = 1;
     r.h.ch = l_row;
     r.h.cl = l_col;
     r.h.dh = r_row;
@@ -29,19 +23,20 @@ void displayColorCombinations(int x1, int y1, int x2, int y2, double T) {
     const int colorCount = 16;
     int height = y2 - y1 + 1;
     window(x1, y1, x2, y2);
-    for (int bg = 0; bg < colorCount; bg++) {
+    for (int bg = 0; bg < colorCount/2; bg++) {
         for (int fg = 0; fg < colorCount; fg++) {
             textbackground(bg);
             textcolor(fg);
-            if (row >= height) {
-                scroll(0, y1, x1, y2, x2, bg);
-                row = 0;
+            if (row > height) {
+                scrollDown(y1-1, x1-1, y2-1, x2-1, bg);
+                row--;
             }
-            gotoxy(1, 1 + row);
+            gotoxy(1, (row % height) + 1);
             clreol();
-            cprintf(" BС: %s, FС: %s", colorNames[bg], colorNames[fg]);
+            cprintf("BC: %s, FC: %s", colorNames[bg], colorNames[fg]);
             row++;
             delay(T * 1000);
+
         }
     }
 }
@@ -49,6 +44,9 @@ void displayColorCombinations(int x1, int y1, int x2, int y2, double T) {
 int main() {
     clrscr();
     displayColorCombinations(25, 5, 55, 15, 0.6);
+    gotoxy(1, 1);
+    cprintf("Program finished. Press any key to exit...");
     getch();
+    clrscr();
     return 0;
 }
